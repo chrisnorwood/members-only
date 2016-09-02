3f59521d4b89114d0d724df67e11e85e4cf188f4
Chris Norwood <cnorwood92@gmail.com>
2016-09-02 10:08:07 -0700

    Basic auth system in place, with remember me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save :downcase_email
 
   validates :name,  presence: true, length: { maximum: 50 }
@@ -6,6 +8,35 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Remember the user in the database, for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Forget a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  # Return true if remember token matches digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Encrypts given token for use in digest
+  def User.digest(token)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(token, cost: cost)
+  end
+
+  # Generates a new, random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
   private
 
