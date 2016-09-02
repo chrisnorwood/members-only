f63ea0e8365a97957656bd7e670885bb85f5c52e
Chris Norwood <cnorwood92@gmail.com>
2016-09-02 12:19:10 -0700

    Final layout touches and db seeds

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      flash[:success] = 'Logged in.'
       redirect_to root_url
     else
       flash.now[:danger] = 'Invalid email/password combination.'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  'Chris', 
+             email: 'chris@example.com',
+             password:              'foobar',
+             password_confirmation: 'foobar')
+
+User.create!(name:  'Joshua', 
+             email: 'josh@example.com',
+             password:              'foobar',
+             password_confirmation: 'foobar')
+
+User.create!(name:  'Melissa', 
+             email: 'melissa@example.com',
+             password:              'foobar',
+             password_confirmation: 'foobar')
+
+User.create!(name:  'Timoteas', 
+             email: 'tim@example.com',
+             password:              'foobar',
+             password_confirmation: 'foobar')
+
+Post.create(user_id: 1,
+            body: 'Sneaky sneaky sneakrets!!!')
+
+Post.create(user_id: 3,
+            body: 'Why u do it mang!!')
+
+Post.create(user_id: 4,
+            body: 'Uh oh.  I think I figured out who posted these.')
+
+Post.create(user_id: 2,
+            body: "Truth be told, I don't even like bananas.")
+
+Post.create(user_id: 1,
+            body: "Slippery slippery slope writing such nonsense.")
